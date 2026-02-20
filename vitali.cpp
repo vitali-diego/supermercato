def6793ba0e60398544ad9611db85f810a14bd6c
vitali <vitali.diego.studente@itispaleocapa.it>
2026-02-20 10:24:30 +0100

    Add files via upload

---
+++ vitali.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   // crea una struttura con tre campi nome,prodotto,prezzo,categoria individua prodotto e array di prodotti  
+   struct prodotto {
+		string nome;
+		string categoria;
+		float prezzo;
+		
+   
+   }
+	int prodotti[];
+   string prodotto;
+    return 0;
+}
+//{}
