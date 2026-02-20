2b843b8191f5a4242f8bf83554fe5de86c8413b0
vitali <vitali.diego.studente@itispaleocapa.it>
2026-02-20 12:00:19 +0100

    Add files via upload

--- vitali.cpp
+++ vitali.cpp
@@ -9,9 +9,19 @@ struct prodotto {
    };
    
    
-   
+//aggiungi variabili e dimensione una alla volta
 void aggiungi (prodotto y,prodotto x[],int d){
+	struct prodotto c;
 	
+	cout<<"inserisci il nome del prodotto :";
+	cin>>c.nome;
+	cout<<endl;
+	cout<<"inserisci la categoria del prodotto :";
+	cin>>c.categoria;
+	cout<<endl;
+	cout<<"inserisci il prezzo del prodotto :";
+	cin>>c.prezzo;
+	cout<<endl;
 	
 	
 }
@@ -56,9 +66,11 @@ bool modifica (prodotto y, prodotto x[], int d ){
 int main()
 {
    // crea una struttura con tre campi nome,prodotto,prezzo,categoria individua prodotto e array di prodotti  
-	int prodotti[100];
+	prodotto sup[100];
+	int d = 0;
 	int n=0;
 	int opzione;
+	prodotto c;
 	do{
 		//visualizzazione voci
 		cout<<"1 - inserimento"<<endl;
@@ -66,14 +78,13 @@ int main()
 		cout<<"3 - cerca"<<endl;
 		cout<<"4 - cancella"<<endl;
 		cout<<"5 - modifica"<<endl;
-		cout<<"6 - esci"<<endl;
+		cout<<"0 - esci"<<endl;
 		
 		cin>>opzione;
 		
 		switch(opzione){
 			case 1:
-			    
-				aggiungi(,prodotti[], int d = 100);
+				aggiungi( c, sup, d);
 				break;
 			case 2:
 			    
@@ -81,18 +92,16 @@ int main()
 				break;
 			case 3:
 			    
-				cerca( prodotti[], int d = 100);
+				cerca(,prodotti, int d = 100);
 				break;
 			case 1:
 			    
-				aggiungi(,prodotti[], int d = 100);
+				aggiungi(,prodotti, int d = 100);
 				break;
 			case 1:
 			    
-				aggiungi(,prodotti[], int d = 100);
+				aggiungi(,prodotti, int d = 100);
 				break;
-			case 6:
-				break;	
 				
 		}
 		
